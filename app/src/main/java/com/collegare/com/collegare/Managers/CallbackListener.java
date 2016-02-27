ffb041f15d0ed625dc41726e568bb42f480237b7
bxute <ankit.kumar071460@gmail.com>
2016-02-27 13:19:53 +0530

    added interface

---
+++ app/src/main/java/com/collegare/com/collegare/Managers/CallbackListener.java
@@ -0,0 +1,8 @@
+package com.collegare.com.collegare.Managers;
+
+/**
+ * Created by Radhe on 27-02-2016.
+ */
+public interface CallbackListener {
+    public void Sent(int type);
+}
---
+++ app/src/main/java/com/collegare/com/collegare/Managers/RefressListener.java
@@ -0,0 +1,8 @@
+package com.collegare.com.collegare.Managers;
+
+/**
+ * Created by Radhe on 27-02-2016.
+ */
+public interface RefressListener {
+    public void refress();
+}
