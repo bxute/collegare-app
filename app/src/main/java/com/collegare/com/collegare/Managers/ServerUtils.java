f3f8a65ec143c77cd59c149bc0b6c5af4ae32900
bxute <ankit.kumar071460@gmail.com>
2016-03-02 12:08:08 +0530

    bug fixation

---
+++ app/src/main/java/com/collegare/com/collegare/Managers/ServerUtils.java
@@ -0,0 +1,12 @@
+package com.collegare.com.collegare.Managers;
+
+/**
+ * Created by Radhe on 02-03-2016.
+ */
+public class ServerUtils {
+
+    public void post(){}
+    public void register(){}
+    public void deregister(){}
+
+}
