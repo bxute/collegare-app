6b8ae5d8164be66b32917eb7bd01cb11128ac0cf
bxute <ankit.kumar071460@gmail.com>
2016-03-03 12:53:14 +0530

    Image Upload Fixation

--- app/src/main/java/com/collegare/com/collegare/Managers/ImageUploader.java
+++ app/src/main/java/com/collegare/com/collegare/Managers/ImageUploader.java
@@ -1,19 +1,21 @@
 package com.collegare.com.collegare.Managers;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStreamReader;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.util.Base64;
 import android.util.Log;
-
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.collegare.com.collegare.Models.CollegareUser;
-
-import java.io.ByteArrayOutputStream;
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.ByteArrayBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
  * Created by Radhe on 03-03-2016.
@@ -44,35 +46,41 @@ public class ImageUploader {
         return encodedImage;
     }
 
-    public void Upload( final Bitmap bmp){
 
+    public void Upload( final Bitmap bmp) {
+       /* CollegareUser user= DatabaseManager.getInstance(context).getUser();
+            try {
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                bmp.compress(Bitmap.CompressFormat.JPEG, 75, bos);
+                byte[] data = bos.toByteArray();
+                HttpClient httpClient = new DefaultHttpClient();
+                String url=App_Config.USER_URL+"?action=setpic&amp;id="+user.id+"&amp;token="+user.token;
+                HttpPost postRequest = new HttpPost(url);
+                ByteArrayBody bab = new ByteArrayBody(data, user.username+".jpg");
+                // File file= new File("/mnt/sdcard/forest.png");
+                // FileBody bin = new FileBody(file);
+                MultipartEntity reqEntity = new MultipartEntity(
+                        HttpMultipartMode.BROWSER_COMPATIBLE);
+                reqEntity.addPart("uploaded", bab);
+                reqEntity.addPart("photoCaption", new StringBody(user.username));
+                postRequest.setEntity(reqEntity);
+                HttpResponse response = httpClient.execute(postRequest);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        response.getEntity().getContent(), "UTF-8"));
+                String sResponse;
+                StringBuilder s = new StringBuilder();
 
-        StringRequest upReq= new StringRequest(StringRequest.Method.POST, App_Config.USER_URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String s) {
-                Log.e("IU",s);
+                while ((sResponse = reader.readLine()) != null) {
+                    s = s.append(sResponse);
+                }
+                Log.e("IU",""+s);
+            } catch (Exception e) {
+                // handle exception here
+                Log.e(e.getClass().getName(), e.getMessage());
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
 
-            }
-        }){@Override
-            protected Map<String ,String> getParams() {
-            CollegareUser user= DatabaseManager.getInstance(new Contexter().getContext()).getUser();
-            Map<String ,String > valuse= new HashMap<>();
-            String img= getStringImage(bmp);
-            Log.e("IU","img string>"+img);
-            valuse.put("action","setpic");
-            valuse.put("id",user.id);
-            valuse.put("token",user.token);
-
-
-            return valuse;
-        }
-        };
-
-    AppManager.getInstance().addToRequestQueue(upReq,"imgreq",context);
+    }
+*/
     }
 
 }
