35750c31d6a11a95967000146313e14f1b5fe519
bxute <ankit.kumar071460@gmail.com>
2016-08-19 09:19:57 +0530

    add: parsers

---
+++ app/src/main/java/com/collegare/com/collegare/Parsers/StatusModel.java
@@ -0,0 +1,14 @@
+
+class StatusModel {
+
+	public String reason;
+	public String value;
+
+	public StatusModel(String reason, String value) {
+
+		this.reason = reason;
+		this.value = value;
+
+	}
+
+}
