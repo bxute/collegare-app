1da9971147d2c679596aa18eb54d73dbae2119e1
bxute <ankit.kumar071460@gmail.com>
2016-08-19 09:11:56 +0530

    add: parsers

---
+++ app/src/main/java/com/collegare/com/collegare/Parsers/MetadataModel.java
@@ -0,0 +1,14 @@
+
+class MetadataModel {
+
+	public String result_type;
+	public int recent_retweets;
+
+	public MetadataModel(String result_type, int recent_retweets) {
+
+		this.result_type = result_type;
+		this.recent_retweets = recent_retweets;
+
+	}
+
+}
