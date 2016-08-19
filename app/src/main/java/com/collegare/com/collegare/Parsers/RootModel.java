83bd4172d47159ee873298b15b927f9079c9ceed
bxute <ankit.kumar071460@gmail.com>
2016-08-19 09:10:39 +0530

    add: parsers

---
+++ app/src/main/java/com/collegare/com/collegare/Parsers/RootModel.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class RootModel {
+
+	public ArrayList<ColorsArrayModel> colorsArray;
+
+	public RootModel(ArrayList<ColorsArrayModel> colorsArray) {
+
+		this.colorsArray = colorsArray;
+
+	}
+
+}
