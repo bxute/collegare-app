f16385feac1604c4a6b0bd341a1fceeddf7f8b64
bxute <ankit.kumar071460@gmail.com>
2016-08-19 09:34:00 +0530

    fix: parsers

--- app/src/main/java/com/collegare/com/collegare/Parsers/AddressModelParser.java
+++ app/src/main/java/com/collegare/com/collegare/Parsers/AddressModelParser.java
@@ -13,7 +13,7 @@ class AddressModelParser {
 			try {
 					JSONObject jsobj = new JSONObject(json_object);
 
-					local_model = new AddressModel(jsobj.getString("streetAddress") , jsobj.getString("postalCode") , jsobj.getString("city") , jsobj.getString("state") , );
+					local_model = new AddressModel(jsobj.getString("state") , jsobj.getString("postalCode") , jsobj.getString("city") , jsobj.getString("streetAddress") , );
  			} 
 			catch (JSONException e){
 
